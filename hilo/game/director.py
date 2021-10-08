c444519fa68d89e01559cddec3854ae8d3959bc1
dcarsin <dcarsin@byui.edu>
2021-10-08 13:29:04 -0300

    logic modifications

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,70 +1,79 @@
 from game.turner import Turner
 
+    #this will check the entered options and reject not allowed
+def check_option(opt1, opt2, question):
+    choice = "x"
+    while choice != opt1 and choice != opt2:
+        choice = input(f"{question} [{opt1}/{opt2}] ").lower()
+        if choice != opt1 and choice != opt2:
+            print(f"{choice} is not allowed, try again...")
+    return choice
+
 class Director:
-    # A code template for a person who directs the game. The responsibility of
-    # this class of objects is to keep track of the score and control the
-    # sequence of play.
-    # Attributes:
-    #     keep_playing (boolean): Whether or not the player wants to keep playing.
-    #     score (number): The total number of points earned.
-    #     turner (Turner): An instance of the class of objects known as Turner.
+    """A code template for a person who directs the game. The responsibility of
+    this class of objects is to keep track of the score and control the
+    sequence of play.
+    Attributes:
+        keep_playing (boolean): Whether or not the player wants to keep playing.
+        points
+        score (number): The total number of points earned.
+        turner (Turner): An instance of the class of objects known as Turner."""
     
     def __init__(self):
-        # The class constructor.
-        # Args:
-        #     self (Director): an instance of Director.
+        """The class constructor.
+        Args:
+            self (Director): an instance of Director."""
         self.keep_playing = True
         self.score = 300
         self.turner = Turner()
+        self.guess = ""
+
 
     def start_game(self):
-        # Starts the game loop to control the sequence of play.
-        # Args:
-        #     self (Director): an instance of Director.
+        """Starts the game loop to control the sequence of play.
+        Args:
+            self (Director): an instance of Director."""
         
         while self.keep_playing:
             self.get_inputs()
-            self.do_updates()
             self.do_outputs()
 
     def get_inputs(self):
-        # Gets the inputs at the beginning of each round of play. In this case,
-        # that means throwing the dice.
-        # Args:
-        #     self (Director): An instance of Director.
-        self.turner
-        self.turner.turner_card()
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means asking for the guess to the user.
+        Args:
+            self (Director): An instance of Director."""
 
-    def do_updates(self):
-        # Updates the important game information for each round of play. In
-        # this case, that means updating the score.
-        # Args:
-        #     self (Director): An instance of Director.
-        points = self.turner.get_points()
-        self.score += points
-        if points <= 0:
-            print()
-            print()
-            print("Better luck next time!")
-            self.score = 0
+        print(f"\nThe card is: {self.turner.first_card}")
+        self.guess = check_option("h", "l", "Make your guess. Will the next card be higher or lower?")        
+
+    # Python program to print
+    # colored text and background    
 
     def do_outputs(self):
-        # Outputs the important game information for each round of play. In
-        # this case, that means the dice that were rolled and the score.
-        # Args:
-        #     self (Director): An instance of Director.
+        """Outputs the important game information for each round of play. In
+        this case, that means the card is turned and the score changes. 
+        (see color)
+        Args:
+            self (Director): An instance of Director."""
+        points = self.turner.get_points(self.guess.lower())
+        self.score += points
+        print(f"The next card was: {self.turner.second_card}")
 
-        print(f"\nYou card is: {self.turner.card}")
-        print(f"Your score is: {self.score}")
-
-        if self.turner.can_throw():
-            choice = input("Roll again? [y/n] ")
-            self.keep_playing = (choice == "y")
+        if points >= 100:
+            print("\x1b[32mCongratulations! You guessed correctly and earned 100 points\x1b[0m")
         else:
+            print("\x1b[31mOh no! Your guess is incorrect and you lose 75 points.\x1b[0m")
+        print(f"Your running score is: {self.score}")
+
+        if self.score > 0:
+            choice = check_option("y", "n", "Keep playing?")
+            if choice == "y":
+                self.keep_playing = True
+
+        if self.score < 0 or choice == "n":
+            print(f"\nThank you for playing! Your final score is: {self.score} points")
             self.keep_playing = False
-            print("Better luck next time!")
-
-
 
 
 
--- hilo/game/turner.py
+++ hilo/game/turner.py
@@ -1,3 +1,59 @@
+import random
+
+# TODO: Define the Thrower class here.
+
+class Turner:
+    """ The turner is the player of the game. The turner must turn the card to play.
+
+        Attributes:
+            first_card (number): Random number between 1 and 13
+            second_card (number): Random number between 1 and 13
+            points (num): Value of the points calculated on each turn
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Turner): an instance of Turner
+
+        """
+        self.first_card = random.randint(1, 13)
+        self.second_card = 0
+        self.points = 0
 
 
-turner_card(card)
+    def get_points(self, guess):
+        """The get_points method calculates and returns the total points for the current 
+        guess. If the person guesses correctly, it will receive 100 points. If it 
+        guesses incorrectly, it will lose 75 points.
+        
+        Args: 
+            self (Turner): an instance of Turner
+            guess (string): the user's guess [h/l]
+        """
+        second_card = self.turn_card()
+
+        if (second_card > self.first_card and guess == 'h') or (second_card < self.first_card and guess == 'l'):
+            self.points = 100
+        else:
+            self.points = -75
+        
+        self.first_card = self.second_card
+        return self.points
+
+
+    def turn_card(self):
+        """The turn_card method randomly generates a new card for the game.
+        
+        Args: 
+            self (Turner): an instance of Turner
+        """
+        
+        self.second_card = random.randint(1, 13)
+        while self.second_card == self.first_card:
+            self.second_card = random.randint(1, 13)
+
+        return self.second_card
+
+        
